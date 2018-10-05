db91b70e76fa97ac9ea4d38c15d43c7077a2aec8
cantstumpthetrump2000 <camerondrain@gmail.com>
2018-10-05 19:24:44 +0100

    new code

---
+++ open_training_data.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+train_data="./sick_train/SICK_train.txt"
+
+file=open(train_data,"r")
+
+file=file.read()
+
+data=file.split("\n")
+
+#[0] heater name not needed
+#print(data[1])
+
+print("last entey",data[-1])
+
+train_data={}
+for q in data[1:-1]:
+    temp=q.split("	")
+    #print(temp[0])
+    key=int(temp[0])
+    data=temp[1:]
+    train_data[key]=data
+
+#print((train_data[10000]))
+#print("done")
+
+
